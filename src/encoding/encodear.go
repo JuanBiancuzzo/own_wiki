6bc5acb36c786ab87d39607417b84c6cdb70e66d
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2025-08-10 11:26:30 -0300

    Creando una forma en la que podemos especificar las tablas en un archivo json

--- src/encoding/encodear.go
+++ src/encoding/encodear.go
@@ -2,10 +2,15 @@ package encoding
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
 	fs "own_wiki/encoding/fs"
 	b "own_wiki/system_protocol/bass_de_datos"
 	d "own_wiki/system_protocol/dependencias"
+
+	_ "embed"
+	"encoding/json"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -13,6 +18,106 @@ import (
 
 // mdp "github.com/gomarkdown/markdown/parser"
 // tp "github.com/BurntSushi/toml"
+
+//go:embed tablas.json
+var infoTablas string
+
+type InfoTabla struct {
+	Nombre           string                `json:"nombre"`
+	Independiente    bool                  `json:"independiente"`
+	Dependible       bool                  `json:"dependible"`
+	ElementosUnicos  bool                  `json:"elementosUnicos"`
+	ValoresGuardar   []InfoValorGuardar    `json:"valoresGuardar"`
+	ReferenciasTabla []InfoReferenciaTabla `json:"referenciasTabla"`
+}
+
+type InfoValorGuardar struct {
+	Clave          string `json:"clave"`
+	Tipo           string `json:"tipo"`
+	Largo          int    `json:"largo"`
+	Representativo bool   `json:"representativo"`
+	Necesario      bool   `json:"necesario"`
+}
+
+type InfoReferenciaTabla struct {
+	Tabla string `json:"tabla"`
+	Clave string `json:"clave"`
+}
+
+func CrearTablas() ([]d.DescripcionTabla, error) {
+	tablas := []d.DescripcionTabla{}
+
+	decodificador := json.NewDecoder(strings.NewReader(infoTablas))
+
+	// read open bracket
+	if _, err := decodificador.Token(); err != nil {
+		return tablas, err
+	}
+
+	mapaTablas := make(map[string]*d.DescripcionTabla)
+
+	for decodificador.More() {
+		var info InfoTabla
+		err := decodificador.Decode(&info)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var nuevaTabla d.DescripcionTabla
+		mapaTablas[info.Nombre] = &nuevaTabla
+
+		var tipoTabla d.TipoTabla = d.DEPENDIENTE_NO_DEPENDIBLE
+		if info.Independiente && info.Dependible {
+			tipoTabla = d.INDEPENDIENTE_DEPENDIBLE
+		} else if info.Independiente && !info.Dependible {
+			tipoTabla = d.INDEPENDIENTE_NO_DEPENDIBLE
+		} else if !info.Independiente && info.Dependible {
+			tipoTabla = d.DEPENDIENTE_DEPENDIBLE
+		}
+
+		paresClaveTipo := []d.ParClaveTipo{}
+		for _, vg := range info.ValoresGuardar {
+			var nuevoClaveTipo d.ParClaveTipo
+
+			switch vg.Tipo {
+			case "string":
+				nuevoClaveTipo = d.NewClaveString(vg.Representativo, vg.Clave, uint(vg.Largo), vg.Necesario)
+
+			case "int":
+				nuevoClaveTipo = d.NewClaveInt(vg.Representativo, vg.Clave, vg.Necesario)
+
+			default:
+				return tablas, fmt.Errorf("el tipo de dato %s no existe, debe ser un error", vg.Tipo)
+			}
+
+			paresClaveTipo = append(paresClaveTipo, nuevoClaveTipo)
+		}
+
+		referenciasTablas := []d.ReferenciaTabla{}
+		for _, rt := range info.ReferenciasTabla {
+			if tabla, ok := mapaTablas[rt.Tabla]; !ok {
+				nombreTablas := []string{}
+				for nombreTabla := range mapaTablas {
+					nombreTablas = append(nombreTablas, nombreTabla)
+				}
+				return tablas, fmt.Errorf("la tabla %s no esta registrada, esto puede ser un error de tipeo, ya que el resto de las tablas son: [%s]", rt.Tabla, strings.Join(nombreTablas, ", "))
+			} else {
+				nuevaReferencia := d.NewReferenciaTabla(rt.Clave, *tabla)
+				referenciasTablas = append(referenciasTablas, nuevaReferencia)
+			}
+		}
+
+		nuevaTabla = d.ConstruirTabla(info.Nombre, tipoTabla, info.ElementosUnicos, paresClaveTipo, referenciasTablas)
+		tablas = append(tablas, nuevaTabla)
+	}
+
+	// read closing bracket
+	if _, err := decodificador.Token(); err != nil {
+		return tablas, err
+	}
+
+	return tablas, nil
+}
 
 func Encodear(dirInput string, canalMensajes chan string) {
 	_ = godotenv.Load()
@@ -32,46 +137,12 @@ func Encodear(dirInput string, canalMensajes chan string) {
 	defer b.CerrarBddNoSQL(bddNoSQL)
 	canalMensajes <- "Se conectaron correctamente las bdd necesarias"
 
-	// Hacer que lo lea del .json q se hizo, para crear las tablas -> en el futuro crear un
-	// lenguaje o una herramienta visual
-	archivos := d.ConstruirTabla(fs.TABLA_ARCHIVOS, d.INDEPENDIENTE_DEPENDIBLE, false, []d.ParClaveTipo{
-		d.NewClaveString(true, "path", 400, true),
-	}, []d.ReferenciaTabla{})
-
-	tags := d.ConstruirTabla(fs.TABLA_TAGS, d.DEPENDIENTE_NO_DEPENDIBLE, true, []d.ParClaveTipo{
-		d.NewClaveString(true, "tag", 255, true),
-	}, []d.ReferenciaTabla{
-		d.NewReferenciaTabla("refArchivo", archivos),
-	})
-
-	personas := d.ConstruirTabla(fs.TABLA_PERSONAS, d.INDEPENDIENTE_DEPENDIBLE, true, []d.ParClaveTipo{
-		d.NewClaveString(true, "nombre", 255, true),
-		d.NewClaveString(true, "apellido", 255, true),
-	}, []d.ReferenciaTabla{})
-
-	editoriales := d.ConstruirTabla(fs.TABLA_EDITORIALES, d.INDEPENDIENTE_DEPENDIBLE, true, []d.ParClaveTipo{
-		d.NewClaveString(true, "editorial", 255, true),
-	}, []d.ReferenciaTabla{})
-
-	libros := d.ConstruirTabla(fs.TABLA_LIBROS, d.DEPENDIENTE_DEPENDIBLE, true, []d.ParClaveTipo{
-		d.NewClaveString(true, "titulo", 255, true),
-		d.NewClaveString(true, "subtitulo", 255, false),
-		d.NewClaveInt(true, "anio", true),
-		d.NewClaveInt(true, "edicion", true),
-		d.NewClaveInt(true, "volumen", true),
-		d.NewClaveString(false, "url", 255, false),
-	}, []d.ReferenciaTabla{
-		d.NewReferenciaTabla("refArchivo", archivos),
-		d.NewReferenciaTabla("refEditorial", editoriales),
-	})
-
-	autoresLibro := d.ConstruirTabla(fs.TABLA_AUTORES_LIBRO, d.DEPENDIENTE_NO_DEPENDIBLE, true, []d.ParClaveTipo{},
-		[]d.ReferenciaTabla{
-			d.NewReferenciaTabla("refLibro", libros),
-			d.NewReferenciaTabla("refPersona", personas),
-		})
-
-	tablas := []d.DescripcionTabla{archivos, tags, personas, editoriales, libros, autoresLibro}
+	tablas, err := CrearTablas()
+	if err != nil {
+		canalMensajes <- fmt.Sprintf("No se pudo crear las tablas, se tuvo el error: %v", err)
+		return
+	}
+	canalMensajes <- "Se leyeron correctamente las tablas"
 
 	tracker, err := d.NewTrackerDependencias(b.NewBdd(bddRelacional, bddNoSQL), tablas, canalMensajes)
 	if err != nil {
