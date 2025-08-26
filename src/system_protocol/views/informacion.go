7e4ad9af64181f67916b6dfe7b44be18e45717b8
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2025-08-26 08:50:23 -0300

    Agregando la forma de hacer una query simple

--- src/system_protocol/views/informacion.go
+++ src/system_protocol/views/informacion.go
@@ -10,6 +10,10 @@ import (
 
 type FnInformacion func(*b.Bdd, []string) (any, error)
 
+func deepCopyDatos(datos d.ConjuntoDato) d.ConjuntoDato {
+	return datos
+}
+
 func NewInformacionFila(query d.QueryDato, parametrosEsperados []string, pametroParaId string) (FnInformacion, error) {
 	indiceIdNecesario := slices.Index(parametrosEsperados, pametroParaId)
 
@@ -26,6 +30,28 @@ func NewInformacionFila(query d.QueryDato, parametrosEsperados []string, pametro
 		}
 	}
 
+	resultado := make(d.ConjuntoDato)
+	posicionResultados := make([]*d.ConjuntoDato, len(infoVariables))
+	for i, info := range infoVariables {
+		posicionResultados[i] = &resultado
+
+		for _, path := range info.Path {
+			if dato, ok := (*posicionResultados[i])[path]; !ok {
+				nuevoConjunto := make(d.ConjuntoDato)
+				ptrNuevoConjunto := &nuevoConjunto
+
+				(*posicionResultados[i])[path] = *ptrNuevoConjunto
+				posicionResultados[i] = ptrNuevoConjunto
+
+			} else if lugar, ok := dato.(d.ConjuntoDato); !ok {
+				return nil, fmt.Errorf("se construyó mal el conjunto de datos para devolver al usuario")
+
+			} else {
+				posicionResultados[i] = &lugar
+			}
+		}
+	}
+
 	return func(bdd *b.Bdd, parametrosDados []string) (any, error) {
 		fila := bdd.MySQL.QueryRow(query.Select, parametrosDados[indiceIdNecesario])
 
@@ -33,77 +59,19 @@ func NewInformacionFila(query d.QueryDato, parametrosEsperados []string, pametro
 			return nil, fmt.Errorf("en lectura de una fila, con la query %s, se tuvo el error: %v", query.Select, err)
 		}
 
-		resultado := make(d.ConjuntoDato)
 		for i, info := range infoVariables {
-			subresultado := &resultado
-
-			for _, path := range info.Path {
-				if dato, ok := (*subresultado)[path]; !ok {
-					nuevoConjunto := make(d.ConjuntoDato)
-					ptrNuevoConjunto := &nuevoConjunto
-
-					(*subresultado)[path] = *ptrNuevoConjunto
-					subresultado = ptrNuevoConjunto
-
-				} else if lugar, ok := dato.(d.ConjuntoDato); !ok {
-					return nil, fmt.Errorf("se construyó mal el conjunto de datos para devolver al usuario")
-
-				} else {
-					subresultado = &lugar
-				}
-			}
-
 			if valor, err := info.Variable.Desreferenciar(datosReferencias[i]); err != nil {
 				return nil, fmt.Errorf("no se pudo desreferenciar %s.%s por: %v", strings.Join(info.Path, "."), info.Nombre, err)
 
 			} else {
+				subresultado := posicionResultados[i]
 				(*subresultado)[info.Nombre] = valor
 			}
 		}
 
+		// No se necesita usar deepCopy porque solo se usa un elemento a la vez, por lo que
+		//   realmente se puede pisar los valores viejos y no deberia haber problema, ya que se crea
+		//   una de estas funciones por cada variable, por lo que no se reutiliza en otro lado
 		return resultado, nil
 	}, nil
 }
-
-/*
-type InformacionTabla struct {
-	Ruta        string
-	Query       d.FnMultiplesDatos
-	Referencias map[string]InformacionReferencia
-}
-
-func NewInformacionTabla(ruta string, query d.FnMultiplesDatos, referencias map[string]InformacionReferencia) InformacionTabla {
-	return InformacionTabla{
-		Ruta:        ruta,
-		Query:       query,
-		Referencias: referencias,
-	}
-}
-
-type InformacionReferencia struct {
-	View       string
-	Parametros map[string]string
-}
-
-func NewInformacionReferencia(view string, parametros map[string]string) InformacionReferencia {
-	return InformacionReferencia{
-		View:       view,
-		Parametros: parametros,
-	}
-}
-
-func (i InformacionTabla) ObtenerInformacion(bdd *b.Bdd, requisitos map[string]string) (any, error) {
-	listaConjuntos, err := i.Query(bdd, requisitos)
-	if err != nil {
-		return nil, fmt.Errorf("se tuvo un error obtener valores, con error: %v", err)
-	}
-
-	datos := make([]d.ConjuntoDato, len(listaConjuntos))
-	for idx, conjuntoDato := range listaConjuntos {
-		datos[idx] = conjuntoDato
-	}
-
-	return datos, nil
-}
-
-*/
