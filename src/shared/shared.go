4d2a9388cc3c3b973661d8e24d9c70ded158d5d8
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2025-12-27 22:57:06 -0300

    Definiendo la interfaz que el usuario deberia rellenar para poder usar el programa

--- src/shared/shared.go
+++ src/shared/shared.go
@@ -1,1 +1,37 @@
 package shared
+
+import (
+	"reflect"
+)
+
+type ComponentInformation reflect.Type
+
+type EntityInformation reflect.Type
+type Entity struct {
+	// Entity struct register with each component fill
+	Entity     any
+	IsMainManu bool
+}
+
+type ViewInformation reflect.Type
+
+/*
+This interface lets the user define the components, each entity and view for the project.
+*/
+type UserDefineStructure interface {
+	// The components are the smallest data storage given by the system. They can depende on
+	// each other, but there has to be a way to constructe them with out an infinite loop
+	RegisterComponents() []ComponentInformation
+
+	// Entities are the composite of components, which are capable of being shown
+	RegisterEntities() []EntityInformation
+
+	// Views are the representation of a entity to be shown by the program in the platform
+	// define at compilation time
+	RegisterViews() map[EntityInformation]ViewInformation
+
+	// Given that when importing file there has to be a way to transform them in entities, this
+	// is where it happends. This also defines what entity is it wanted to be the main menu. If
+	// multiples entities are main menu capable, then it will apear an option to select
+	ProcessFile(file string) []Entity
+}
