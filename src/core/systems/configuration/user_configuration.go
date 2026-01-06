fdf9338a0a496ad653e8fdf20f874ab02a72d310
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2026-01-06 15:09:50 -0300

    Agregando los archivos de configuración

--- src/core/systems/configuration/user_configuration.go
+++ src/core/systems/configuration/user_configuration.go
@@ -1,12 +1,24 @@
 package configuration
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 var UserConfig UserConfiguration
 
 type UserConfiguration struct {
-	TargetFrameRate int
+	TargetFrameRate uint64 `json:"TargetFPS"`
 }
 
 func LoadUserConfiguration(userConfigurationPath string) error {
+	if userConfigBytes, err := os.ReadFile(userConfigurationPath); err != nil {
+		return fmt.Errorf("failed to read User define configuration, with error: %v\n", err)
+
+	} else if err = json.Unmarshal(userConfigBytes, &UserConfig); err != nil {
+		return err
+	}
 
 	return nil
 }
