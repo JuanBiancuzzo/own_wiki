a90de4161ce9b06b608b14701386d9989b494352
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2026-01-13 17:10:13 -0300

    Seteando la configuracion y el logger para el servidor y el cliente

--- client/main.go
+++ client/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
 
+	c "github.com/JuanBiancuzzo/own_wiki/core/systems/configuration"
+	log "github.com/JuanBiancuzzo/own_wiki/core/systems/logger"
+)
+
+func main() {
+	// Command arguments parsing
+	pathConfig := flag.String("config_path", "", "This is the path to the config file")
+	flag.Parse()
+
+	configuration, err := c.NewConfiguration(*pathConfig)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to load the configuration at %q, with error: %v", *pathConfig, err)
+		return
+	}
+
+	if err := log.CreateLogger(configuration.Logger); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create the logger with (%s), with error: %v", configuration.Logger.String(), err)
+		return
+	}
+	defer log.Close()
 }
--- core/systems/logger/logger.go
+++ core/systems/logger/logger.go
@@ -7,14 +7,6 @@ import (
 	"sync"
 )
 
-type Verbosity string
-
-const (
-	MUTE    = "mute"
-	NORMAL  = "normal"
-	VERBOSE = "verbose"
-)
-
 type loggerInfo struct {
 	MsgChannel    chan string
 	Verbosity     Verbosity
@@ -24,13 +16,13 @@ type loggerInfo struct {
 
 var logger *loggerInfo = nil
 
-func CreateLogger(path string, verbosity Verbosity, messageCapacity uint) (err error) {
-	if verbosity == MUTE {
+func CreateLogger(config LoggerConfiguration) (err error) {
+	if config.Verbosity == MUTE {
 		// We make nothing if the verbosity is muted
 		return err
 	}
 
-	splitPath := strings.Split(path, "/")
+	splitPath := strings.Split(config.LogPath, "/")
 	folder := strings.Join(splitPath[:len(splitPath)-1], "/")
 
 	if err = os.MkdirAll(folder, os.ModePerm); err != nil {
@@ -38,11 +30,11 @@ func CreateLogger(path string, verbosity Verbosity, messageCapacity uint) (err e
 	}
 
 	var file *os.File = nil
-	if file, err = os.Create(path); err != nil {
+	if file, err = os.Create(config.LogPath); err != nil {
 		return fmt.Errorf("failed to open logger file, with error: %w", err)
 	}
 
-	channel := make(chan string, messageCapacity)
+	channel := make(chan string, int(config.MessageCapacity))
 	var waitWriteFile sync.WaitGroup
 	waitWriteFile.Add(1)
 
@@ -55,7 +47,7 @@ func CreateLogger(path string, verbosity Verbosity, messageCapacity uint) (err e
 
 	logger = &loggerInfo{
 		MsgChannel:    channel,
-		Verbosity:     verbosity,
+		Verbosity:     config.Verbosity,
 		File:          file,
 		WaitWriteFile: &waitWriteFile,
 	}
