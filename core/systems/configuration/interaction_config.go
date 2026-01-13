45c1e7aa03909a2a3b2f29d12758b1ee5455e3e9
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2026-01-13 16:06:31 -0300

    Agregando los tags de json a las configuraciones

--- core/systems/configuration/interaction_config.go
+++ core/systems/configuration/interaction_config.go
@@ -1,13 +1,13 @@
 package configuration
 
 type UserInteractionConfig struct {
-	Protocol string
-	Ip       string
-	Port     uint8
+	Protocol string `json:"protocol"`
+	Ip       string `json:"ip"`
+	Port     uint8  `json:"port"`
 }
 
 type SystemInteractionConfig struct {
-	Protocol string
-	Ip       string
-	Port     uint8
+	Protocol string `json:"protocol"`
+	Ip       string `json:"ip"`
+	Port     uint8  `json:"port"`
 }
