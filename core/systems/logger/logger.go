6617993e55e19618ae760029efd7a7777f29286d
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2026-01-13 18:47:51 -0300

    Haciendo que el logger cree un archivo de json con la informacion detallada

--- core/systems/logger/logger.go
+++ core/systems/logger/logger.go
@@ -1,17 +1,31 @@
 package logger
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"runtime"
 	"strings"
 	"sync"
+	"time"
 )
 
+type messageInfo struct {
+	Time    string `json:"time"`
+	Message string `json:"message"`
+	Level   string `json:"level"`
+	Trace   string `json:"trace"`
+}
+
+type FnCreateMessageInfo func(message, level, filename string, lineNumber int) messageInfo
+
 type loggerInfo struct {
-	MsgChannel    chan string
+	MsgChannel    chan messageInfo
 	Verbosity     Verbosity
 	File          *os.File
 	WaitWriteFile *sync.WaitGroup
+
+	CreateMessage FnCreateMessageInfo
 }
 
 var logger *loggerInfo = nil
@@ -33,14 +47,17 @@ func CreateLogger(config LoggerConfiguration) (err error) {
 	if file, err = os.Create(config.LogPath); err != nil {
 		return fmt.Errorf("failed to open logger file, with error: %w", err)
 	}
+	file.Write([]byte("[\n"))
 
-	channel := make(chan string, int(config.MessageCapacity))
+	channel := make(chan messageInfo, int(config.MessageCapacity))
 	var waitWriteFile sync.WaitGroup
 	waitWriteFile.Add(1)
 
-	go func(messageChannel chan string, wg *sync.WaitGroup) {
+	go func(messageChannel chan messageInfo, wg *sync.WaitGroup) {
 		for message := range messageChannel {
-			file.Write([]byte(message))
+			if byteMessage, err := json.Marshal(message); err == nil {
+				file.Write(append(byteMessage, byte('\n')))
+			}
 		}
 		wg.Done()
 	}(channel, &waitWriteFile)
@@ -50,6 +67,15 @@ func CreateLogger(config LoggerConfiguration) (err error) {
 		Verbosity:     config.Verbosity,
 		File:          file,
 		WaitWriteFile: &waitWriteFile,
+
+		CreateMessage: func(message, level, filename string, lineNumber int) messageInfo {
+			return messageInfo{
+				Message: message,
+				Time:    time.Now().Format(config.DateFormat),
+				Level:   level,
+				Trace:   fmt.Sprintf("In %q, at %d", filename, lineNumber),
+			}
+		},
 	}
 
 	return err
@@ -60,17 +86,12 @@ func Info(format string, args ...any) {
 		return
 	}
 
-	message := fmt.Sprintf(format, args...)
-	logger.MsgChannel <- fmt.Sprintf(" [INFO] %s\n", message)
-}
-
-func Error(format string, args ...any) {
-	if logger == nil {
-		return
+	if _, filePath, lineNumber, ok := runtime.Caller(1); ok {
+		logger.MsgChannel <- logger.CreateMessage(
+			fmt.Sprintf(format, args...),
+			"Info", filePath, lineNumber,
+		)
 	}
-
-	message := fmt.Sprintf(format, args...)
-	logger.MsgChannel <- fmt.Sprintf("\033[31m [ERROR] %s\033[39m\n", message)
 }
 
 func Debug(format string, args ...any) {
@@ -78,8 +99,38 @@ func Debug(format string, args ...any) {
 		return
 	}
 
-	message := fmt.Sprintf(format, args...)
-	logger.MsgChannel <- fmt.Sprintf("\033[36m [DEBUG] %s\033[39m\n", message)
+	if _, filePath, lineNumber, ok := runtime.Caller(1); ok {
+		logger.MsgChannel <- logger.CreateMessage(
+			fmt.Sprintf(format, args...),
+			"Debug", filePath, lineNumber,
+		)
+	}
+}
+
+func Error(format string, args ...any) {
+	if logger == nil {
+		return
+	}
+
+	if _, filePath, lineNumber, ok := runtime.Caller(1); ok {
+		logger.MsgChannel <- logger.CreateMessage(
+			fmt.Sprintf(format, args...),
+			"Error", filePath, lineNumber,
+		)
+	}
+}
+
+func Fatal(format string, args ...any) {
+	if logger == nil {
+		return
+	}
+
+	if _, filePath, lineNumber, ok := runtime.Caller(1); ok {
+		logger.MsgChannel <- logger.CreateMessage(
+			fmt.Sprintf(format, args...),
+			"Fatal", filePath, lineNumber,
+		)
+	}
 }
 
 func Close() {
@@ -89,4 +140,6 @@ func Close() {
 
 	close(logger.MsgChannel)
 	logger.WaitWriteFile.Wait()
+
+	logger.File.Write([]byte("[\n"))
 }
