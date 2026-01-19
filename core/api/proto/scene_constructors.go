3c394a811e759b60768260f9d877004a78f4712c
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2026-01-19 11:04:23 -0300

    Agregando los constructores para las escena

---
+++ core/api/proto/scene_constructors.go
@@ -0,0 +1,46 @@
+package core
+
+// ---+--- SceneDescription ---+---
+
+func New2DScene(layout *Layout) *SceneDescription {
+	return &SceneDescription{
+		MainCamera: New2DCamera(layout),
+	}
+}
+
+// ---+--- Camera ---+---
+var IdentityMatrix = []float32{
+	1, 0, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 1, 0,
+	0, 0, 0, 1,
+}
+
+func New2DCamera(layout *Layout) *Camera {
+	return &Camera{
+		Perspective: &Camera_PerspectiveMatrix{
+			Value: IdentityMatrix,
+		},
+		ScreenLayout: layout,
+	}
+}
+
+// ---+--- SceenObject ---+---
+func NewSceenTextObject(text string) *SceneObject {
+	return &SceneObject{
+		Object: &SceneObject_Text{
+			Text: text,
+		},
+	}
+}
+
+func NewSceenImageObject(url, title string) *SceneObject {
+	return &SceneObject{
+		Object: &SceneObject_ImageInfo{
+			ImageInfo: &SceneObject_Image{
+				Url:   url,
+				Title: title,
+			},
+		},
+	}
+}
